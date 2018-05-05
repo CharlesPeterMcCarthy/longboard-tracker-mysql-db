2d9a9f467fc34a69eb182bbee01d9ea227c31395
CharlesPeterMcCarthy <charles@studybundles.com>
2018-05-05 19:07:53 +0100

    Updated Approved Devices Structure

--- approved_devices/approved_devices.sql
+++ approved_devices/approved_devices.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `approved_devices` (
   `device_name` varchar(50) NOT NULL,
   `device_pass` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `password` char(62) NOT NULL,
   PRIMARY KEY (`device_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
--- longboard_db.sql
+++ longboard_db.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS `approved_devices` (
   `device_name` varchar(50) NOT NULL,
   `device_pass` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `password` char(62) NOT NULL,
   PRIMARY KEY (`device_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
